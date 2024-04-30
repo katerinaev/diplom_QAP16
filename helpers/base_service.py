4ded1a354a36e4649a1456dbe7b3d0ce5da7c977
katerinaev <katerina.ev@gmail.com>
2024-04-30 18:23:01 +0300

    -- add flake

--- helpers/base_service.py
+++ helpers/base_service.py
@@ -79,13 +79,3 @@ class BaseServices:
         else:
             self.logger.error('FAIL')
             assert False
-
-    # def _logger(self):
-    #     self.logger = logging.getLogger(self.__class__.__name__)
-    #     self.logger.setLevel(logging.DEBUG)
-    #     handler = StreamHandler()
-    #     handler.setFormatter(Formatter(fmt='%(asctime)s %(levelname)s %(message)s'))
-    #     file_handler = FileHandler('logging_api.log')
-    #     self.logger.addHandler(file_handler)
-    #     self.logger.addHandler(handler)
-
